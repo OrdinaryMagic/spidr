9ba04a086d1a6375b7ace015959f3380ab23e2f0
ordm <ordm67@gmail.com>
2020-05-15 17:11:21 +0300

    fix

--- lib/spidr/agent/sanitizers.rb
+++ lib/spidr/agent/sanitizers.rb
@@ -21,7 +21,7 @@ module Spidr
     # @since 0.2.2
     #
     def sanitize_url(url)
-      url = URI(url)
+      url = URI(url.chomp('/'))
 
       url.fragment = nil if @strip_fragments
       url.query    = nil if @strip_query
